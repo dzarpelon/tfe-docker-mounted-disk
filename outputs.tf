38ac9ff36dcd66fbc738dedec8e3e7b658e7b506
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-04-10 17:28:00 +0200

    show TFE URL on the output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "initial_admin_user_creation_status" {
   description = "The status of the initial admin user creation. Check the /var/log/cloud-init-output.log file on the instance for details."
   value       = "Check the /var/log/cloud-init-output.log file on the instance for the result of the user creation process."
 }
+
+output "tfe_url" {
+  description = "The URL of the Terraform Enterprise system that will be used to access it"
+  value = "https://${var.aws_instance_name}.${var.route53_zone_name}"
+}
