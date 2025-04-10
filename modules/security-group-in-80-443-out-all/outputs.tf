e21a5fd7494bb3e1a5b4db0d9187cddcd9ffc3bc
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-04-10 12:03:30 +0200

    refactor code to have security group renamed so it includes what it does

--- modules/security-group-in-80-443-out-all/outputs.tf
+++ modules/security-group-in-80-443-out-all/outputs.tf
@@ -1,4 +1,4 @@
 output "tfe_security_group_id" {
   description = "The ID of the security group."
-  value       = aws_security_group.tfe_security_group.id
+  value       = aws_security_group.security-group-in-80-443-out-all.id
 }
